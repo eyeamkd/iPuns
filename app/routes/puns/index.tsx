2a73970e4db7b63d64bd4f6aec872a033cca9963
Kunal Dubey <kunaldubey2297@gmail.com>
2021-11-27 23:41:18 +0530

    added data fetching inside loader

--- app/routes/puns/index.tsx
+++ app/routes/puns/index.tsx
@@ -1,6 +1,6 @@
-import  type { LinksFunction } from "remix";
-import React from 'react'; 
-import {Outlet, Link} from "remix"; 
+import  type { LinksFunction ,LoaderFunction } from "remix";
+import {Link,Outlet, useLoaderData} from "remix";  
+import {db} from "~/utils/db.server"; 
 import stylesUrl from "../../styles/puns.css"; 
 
 export let links: LinksFunction = () => {
@@ -10,13 +10,59 @@ export let links: LinksFunction = () => {
             href:stylesUrl
         }
     ]
+}  
+
+type LoaderData = { 
+    punsList : Array<{id:string, name:string}>;
 }
 
-function PunsIndexRoute() {
+export let loader: LoaderFunction = async () => {
+    let puns: LoaderData = {
+        punsList: await db.pun.findMany()
+    };  
+
+    return puns;
+}
+
+function PunsIndexRoute() { 
+    let puns = useLoaderData<LoaderData>()
     return (
-        <div>
-            This is where Puns live
+        <div className="jokes-layout">
+      <header className="jokes-header">
+        <div className="container">
+          <h1 className="home-link">
+            <Link
+              to="/"
+              title="iPuns"
+              aria-label="iPuns"
+            >
+              <span className="logo-medium">iPuns</span>
+            </Link>
+          </h1>
         </div>
+      </header>
+      <main className="jokes-main">
+        <div className="container">
+          <div className="jokes-list">
+            <Link to=".">Get a random Pun</Link>
+            <p>Here are a few more puns to check out:</p>
+            <ul>
+              {puns.punsList.map(pun => (
+                <li key={pun.id}>
+                  <Link to={pun.id}>{pun.name}</Link>
+                </li>
+              ))}
+            </ul>
+            <Link to="new" className="button">
+              Add your own
+            </Link>
+          </div>
+          <div className="jokes-outlet">
+            <Outlet />
+          </div>
+        </div>
+      </main>
+    </div>
     )
 }
 
