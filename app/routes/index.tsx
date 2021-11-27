0d4bb371b9286a8c32bf0ac5d76e854ef7826498
Kunal Dubey <kunaldubey2297@gmail.com>
2021-11-27 22:36:44 +0530

    initial commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,6 @@
+// Hope that it doesn't matter what we have inside Index.tsx ( the component name)
+// this shall always route down to the base "/" route
+
+export default function IndexRoute(){
+    return <div>This is Index Route</div>
+}
